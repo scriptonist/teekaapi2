4bb61dccc56664857b453ce42b912cf087592963
scriptonist <aravind.techbridge@gmail.com>
2016-01-17 23:16:46 +0530

    added stream for one song

--- main.js
+++ main.js
@@ -10,4 +10,23 @@ $(document).ready(function(){
 		  card_activator: 'click' // or hover
 	});
 	$('.tooltipped').tooltip({delay: 50});
+	//Audio Controls
+
+	var churilmudi = new Audio("songs/audio/Vaibhavam_Ninte_Churul_Mudiyil_-_Teekaapi_Offic.mp3");
+	$('#churilmudiyilPlayButton').click(function(){
+		churilmudi.play();
+		$('#startTime').html('0.00');
+		$('#endTime').html(String(Math.round((churilmudi.duration/60)*100)/100));
+		$('#churilmudiProgress').attr('style','width:'+'0'+'%');
+		setInterval(function(){
+			var pro = (churilmudi.currentTime/churilmudi.duration)*100;
+			$('#churilmudiProgress').attr('style','width:'+pro+'%');
+			console.log(pro)
+		}
+		,500);
+	});
+	$('#churilmudiyilPauseButton').click(function(){
+		churilmudi.pause();
+		
+	});
 });
